31f2d5d4a9b4fc1e8f990aa5c76b181e09f2a0d7
ebiliak <ilforoughi@s.sfusd.edu>
2025-04-25 00:39:45 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,22 @@
 public class APLine
 {
-  /* your code here */
+private int a, b, c;
+  Public APLine(int a1, int b1, int c1){
+    a1 = a;
+    b1=b;
+    c1 = c;
+  }
+    
+  public getSlope(int jo, bo){
+                  jo = al;
+    bo = al;
+    return getSlope(jo/bo)
+      } 
+  private isOnLine(int d, l){
+                   if((a(d) +d (l) + c) ==0){
+                     retrun true;
+                   }
+    return false;
+  }
   
 }
